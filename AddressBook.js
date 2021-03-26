7f9c890f68de717a1339cc5429f8eb7a2e3fe1e2
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-26 15:57:17 +0530

    Updated : Ability to ensure that valid contacts are added

--- AddressBook.js
+++ AddressBook.js
@@ -93,9 +93,62 @@ contacts.push(dMart);
 contacts.push(Jio);
 console.log("-----------------------------------------------------------Welcome to Address Book Manangement System--------------------------------------------------------\n");
 
-	for(let i=0;i<contacts.length;i++){
-	    contacts[i].showDetails(); 
-   }
+let check = true;
+
+while(check)
+    {
+        console.log("1.Add contacts");
+        console.log("2.Show Conact details");
+        console.log("3.Exit")
+let choice = Number(prompt('Enter Choice:'));
+
+switch(choice){
+    case 1:{
+        try{
+            let firstName = prompt('First Name:');     
+            if(RegExp('^[A-Z]{1}[a-z]{2,}$').test(firstName)){
+                let lastName  = prompt('Last Name:');
+                if(RegExp('^[A-Z]{1}[a-z]{2,}$').test(lastName)){
+                    let address   = prompt('Address:');  
+                    if(RegExp('^[a-zA-Z]{1}[a-z]{3,}$').test(address)){
+                        let city      = prompt('City:');
+                        if(RegExp('^[a-zA-Z]{1}[a-z]{3,}$').test(city)){
+                            let state     = prompt('State:'); 
+                            if(RegExp('^[a-zA-Z]{1}[a-z]{3,}$').test(state)){
+                                let zip       = prompt('Zip:');
+                                if(RegExp('^[0-9]{6,6}$').test(zip)){
+                                    let phoneNumber = prompt('Phone number:'); 
+                                    if(RegExp('^[1-9]{1}[0-9]{9,9}$').test(phoneNumber)){
+                                        let email       = prompt('Email:'); 
+                                        if(RegExp('^(abc)?(.+)@(.+)([.](com))?([^.])$').test(email)){
+                                                let input = new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
+                                                contacts.push(input);
+                                                console.log("You have succesfully added contact !\n");
+                                        }else throw "Email is incorrect";
+                                    }else throw "Phone number is incorrect";
+                                }else throw "zip is incorrect";
+                            }else throw "state name is incorrect";
+                        }else throw "city name is incorrect";
+                    }else throw "Address is incorrect";
+                }else throw "Last name is incorrect";
+            }else throw "First name is incorrect";
+
+        }catch (e){
+            console.error(e);
+        }   
+        break;
+    }
+    case 2:{
+            for(var element of contacts){
+                element.showDetails();
+            }
+            break;
+    }
+    case 3:
+        check = false;
+        break;
+    }
+}
 
 
     
