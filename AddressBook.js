61592c74b89bc863763b9386aae569d39397ecd3
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-26 19:09:05 +0530

    Updated:Ability to find person with name delete it from array

--- AddressBook.js
+++ AddressBook.js
@@ -24,7 +24,7 @@ class Contact {
     }
     set firstName(name) {
         let firstNameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if(firstNameRegex.test(name))
+        if (firstNameRegex.test(name))
             this._firstName = name;
         else throw "First Name is Incorrect!";
     }
@@ -34,7 +34,7 @@ class Contact {
     }
     set lastName(lastName) {
         let lastNameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if(lastNameRegex.test(lastName))
+        if (lastNameRegex.test(lastName))
             this._lastName = lastName;
         else throw "Last Name is Incorrect!";
     }
@@ -44,7 +44,7 @@ class Contact {
     }
     set address(address) {
         let addressRegex = RegExp('^[a-zA-Z]{1}[a-z]{3,}$');
-        if(addressRegex.test(address))
+        if (addressRegex.test(address))
             this._address = address;
         else throw "Address is Incorrect!";
     }
@@ -54,7 +54,7 @@ class Contact {
     }
     set city(cityName) {
         let cityRegex = RegExp('^[a-zA-Z]{1}[a-z]{3,}$');
-        if(cityRegex.test(cityName))
+        if (cityRegex.test(cityName))
             this._city = cityName;
         else throw "City name is Incorrect!";
     }
@@ -64,7 +64,7 @@ class Contact {
     }
     set state(stateName) {
         let stateRegex = RegExp('^[a-zA-Z]{1}[a-z]{3,}$');
-        if(stateRegex.test(stateName))
+        if (stateRegex.test(stateName))
             this._state = stateName;
         else throw "State name is Incorrect!";
     }
@@ -74,7 +74,7 @@ class Contact {
     }
     set zip(zip) {
         let zipRegex = RegExp('^[0-9]{6,6}$');
-        if(zipRegex.test(zip))
+        if (zipRegex.test(zip))
             this._zip = zip;
         else throw "Zip is Incorrect!";
     }
@@ -84,7 +84,7 @@ class Contact {
     }
     set phoneNumber(phoneNumber) {
         let phoneNumberRegex = RegExp('^[1-9]{1}[0-9]{9,9}$');
-        if(phoneNumberRegex.test(phoneNumber))
+        if (phoneNumberRegex.test(phoneNumber))
             this._phoneNumber = phoneNumber;
         else throw "Phone number is Incorrect!";
     }
@@ -94,16 +94,16 @@ class Contact {
     }
     set email(email) {
         let emailRegex = RegExp('^(abc)?(.+)@(.+)([.](com))?([^.])$');
-        if(emailRegex.test(email))
+        if (emailRegex.test(email))
             this._email = email;
         else throw "Email is Incorrect!";
     }
 
     showDetails() {
-		console.log("First Name: " + this.firstName + "\nLast Name: " + this.lastName + "\nAddress: " + this.address + "\nCity: "+this.city +
-                                                        "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber
-                                                        + "\nEmail: " + this.email + "\n");
-	}
+        console.log("First Name: " + this.firstName + "\nLast Name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city +
+            "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber
+            + "\nEmail: " + this.email + "\n");
+    }
 }
 
 const prompt = require('prompt-sync')();
@@ -112,97 +112,96 @@ let contacts = new Array();
 console.log("-----------------------------------------------------------Welcome to Address Book Manangement System--------------------------------------------------------\n");
 
 let check = true;
-while(check)
-{
-        console.log("1.Add contacts");
-        console.log("2.Show Contact details");
-        console.log("3.Edit contact");
-        console.log("4.Delete contact");
-        console.log("5.Exit");
-let choice = Number(prompt('Enter Choice:'));
-console.log();
-switch(choice){
-    case 1:{
-        try{
-            let firstName = prompt('First Name:');     
-            let lastName  = prompt('Last Name:');
-            let address   = prompt('Address:');              
-            let city      = prompt('City:');
-            let state     = prompt('State:'); 
-            let zip       = prompt('Zip:');
-            let phoneNumber = prompt('Phone number:'); 
-            let email       = prompt('Email:'); 
-            let input = new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
-            contacts.push(input);
-            console.log("You have succesfully added contact !\n");   
-        }catch (e){
-            console.error(e);
-        }   
-        break;
-    }
-    case 2:{
-            for(var contact of contacts){
+while (check) {
+    console.log("1.Add contacts");
+    console.log("2.Show Contact details");
+    console.log("3.Edit contact");
+    console.log("4.Delete contact");
+    console.log("5.Exit");
+    let choice = Number(prompt('Enter Choice:'));
+    console.log();
+    switch (choice) {
+        case 1: {
+            try {
+                let firstName = prompt('First Name:');
+                let lastName = prompt('Last Name:');
+                let address = prompt('Address:');
+                let city = prompt('City:');
+                let state = prompt('State:');
+                let zip = prompt('Zip:');
+                let phoneNumber = prompt('Phone number:');
+                let email = prompt('Email:');
+                let input = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                contacts.push(input);
+                console.log("You have succesfully added contact !\n");
+            } catch (e) {
+                console.error(e);
+            }
+            break;
+        }
+        case 2: {
+            for (var contact of contacts) {
                 contact.showDetails();
             }
             break;
-    }
-    case 3:{
-        let index = 0;
-		let isPresent=false;
-		let tempFirstName= prompt('Enter person name do you want to edit :');
-		for(let i = 0;i < contacts.length ;i++) {
-			if(contacts[i]._firstName == tempFirstName) {
-				isPresent=true;
-                index = i; 
-				break;		
-			}
-		}
-        if(isPresent){
-            contacts[index]._firstName = prompt("First Name:");
-            contacts[index]._lastName  = prompt('Last Name:');
-            contacts[index]._address   = prompt('Address:');              
-            contacts[index]._city      = prompt('City:');
-            contacts[index]._state     = prompt('State:'); 
-            contacts[index]._zip       = prompt('Zip:');
-            contacts[index]._phoneNumber = prompt('Phone number:'); 
-            contacts[index]._email       = prompt('Email:');
-            console.log("You have succesfully edited contact !\n"); 
         }
-        else  console.log("There is no contact of given name.");
+        case 3: {
+            let index = 0;
+            let isPresent = false;
+            let tempFirstName = prompt('Enter person name do you want to edit :');
+            for (let i = 0; i < contacts.length; i++) {
+                if (contacts[i]._firstName == tempFirstName) {
+                    isPresent = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (isPresent) {
+                contacts[index]._firstName = prompt("First Name:");
+                contacts[index]._lastName = prompt('Last Name:');
+                contacts[index]._address = prompt('Address:');
+                contacts[index]._city = prompt('City:');
+                contacts[index]._state = prompt('State:');
+                contacts[index]._zip = prompt('Zip:');
+                contacts[index]._phoneNumber = prompt('Phone number:');
+                contacts[index]._email = prompt('Email:');
+                console.log("You have succesfully edited contact !\n");
+            }
+            else console.log("There is no contact of given name.");
 
-        break;
-    }case 4:{
-        let index = 0;
-		let isPresent=false;
-		let tempFirstName= prompt('Enter person name do you want to delete :');
-		for(let i = 0;i < contacts.length ;i++) {
-			if(contacts[i]._firstName == tempFirstName) {
-				isPresent=true;
-                index = i; 
-				break;		
-			}
-		}
-        if(isPresent){
-            contacts.splice(index,1);
-            console.log("You have succesfully deleted contact !\n");
-        } 
-        else  console.log("There is no contact of given name.");
+            break;
+        } case 4: {
+            let index = 0;
+            let isPresent = false;
+            let tempFirstName = prompt('Enter person name do you want to delete :');
+            for (let i = 0; i < contacts.length; i++) {
+                if (contacts[i]._firstName == tempFirstName) {
+                    isPresent = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (isPresent) {
+                contacts.splice(index, 1);
+                console.log("You have succesfully deleted contact !\n");
+            }
+            else console.log("There is no contact of given name.");
 
-        break;
+            break;
+        }
+        case 5: {
+            check = false;
+            break;
+        }
+        default:
+            console.log("Invalid Choice");
     }
-    case 5:{
-        check = false;
-        break;
-    }
-    default:
-        console.log("Invalid Choice");
- }
 }
 
 
 
 
-    
+
 
 
 
